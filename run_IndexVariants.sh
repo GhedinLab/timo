20787af776501f72e5e8b1698189b6bf5a1528b6
aeroder <allison.roder@nih.gov>
2021-04-02 13:40:10 -0600

    updated run_IndexVariant.sh to general format

--- run_IndexVariants.sh
+++ run_IndexVariants.sh
@@ -2,13 +2,13 @@
 
 #### USER CHANGES THESE VARIABLES DEPENDING ON RUN ######
 ### run: bash ./run_IndexVariants.sh
-JOB_NAME="OMS524-583-NIHvariants"
-RUNDIR="/data/SGSlab/pipelines/timo"
-BAM_DIR="/data/SGSlab/NIH-COV2/20210329/out/merged" #using bam because of index
+JOB_NAME="variants-run"
+RUNDIR="/data/user/timo"
+BAM_DIR="/data/user/bamdir" #using bam because of index
 STRAIN="cov19" #lowercase strain name
-REFSEQ="/data/SGSlab/SARS-CoV2.fa"
-USER='allison.roder@nih.gov' #email for running information
-array=0-43 #number of samples! (not number of bams...) originally 0-21
+REFSEQ="/data/user/SARS-CoV2.fa"
+USER='some.email@email.com' #email for running information
+array=0-9 #number of samples! (not number of bams...)
 
 
 #### DON'T TOUCH BELOW HERE #####
